0eccd0f7ddd203b376ddf1c2e41a4d9e34a14a59
unknown <you@example.com>
2022-07-29 18:01:51 -0400

    07092022 6:00PM

---
+++ src/hw4JavaVariables/AboutMe.java
@@ -0,0 +1,29 @@
+package hw4JavaVariables;
+
+public class AboutMe {
+
+	public String nickname;// variable declared
+	public byte age;// variable declared
+	public short myApertmentRent;// variable declared
+	public int mySalary;// variable declared
+	public long myIdNumber;// variable declared
+	public float myHeight;// variable declared
+	public double myGrade;// variable declared
+	public char gender;// variable declared
+	public boolean married;// variable declared
+
+	public AboutMe() {// Constructor declared if there is no sysout
+		System.out.println("This is all about us:"); // Constructor Initialized
+
+		System.out.println("\n******************************************************\n");
+
+	}
+
+	public void aboutMe() {//method implemented and  concatanation
+		System.out.println("My nick name:" + nickname + "\t\tMy age:" + age + "\t\t\tMy Apartment Rent "
+				+ myApertmentRent + "\nMy Salary:" + mySalary + "\t\t\tMy Id Number:" + myIdNumber + "\tMy height:"
+				+ myHeight + "\nMy Grade:" + myGrade + "\t\tMy Gender:" + gender + "\t\t\tMy Status " + married);
+
+	}
+
+}
---
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -0,0 +1,36 @@
+package hw4JavaVariables;
+
+public class AboutMeTest {
+
+	public static void main(String[] args) {
+		AboutMe aboutMe = new AboutMe();// Constructor initialized
+		aboutMe.nickname = "Boby";// string initialized
+		aboutMe.age = 55;// byte initialized
+		aboutMe.myApertmentRent = 12345;// short initialized
+		aboutMe.mySalary = 2000;// int initialized
+		aboutMe.myIdNumber = 12345678910l;// long initialized
+		aboutMe.myHeight = 5.90f;// float initialized
+		aboutMe.myGrade = 3.51234567890;// double initialized
+		aboutMe.gender = 'M';// char initialized
+		aboutMe.married = true;// boolean initialized
+
+		aboutMe.aboutMe();//method called
+
+		System.out.println("\n****************************************************\n");
+
+		AboutMe aboutAlex = new AboutMe();// Consturctor initialized
+		aboutAlex.nickname = "Alex";// string initialized
+		aboutAlex.age = 50;// byte initialized
+		aboutAlex.myApertmentRent = 5432;// short initialized
+		aboutAlex.mySalary = 5000;// int initialized
+		aboutAlex.myIdNumber = 12345678911l;// long initialized
+		aboutAlex.myHeight = 5.50f;// float initialized
+		aboutAlex.myGrade = 3.41234567890;// double initialized
+		aboutAlex.gender = 'M';// char initialized
+		aboutAlex.married = false;// boolean initialized
+
+		aboutAlex.aboutMe();//method called
+
+	}
+
+}
